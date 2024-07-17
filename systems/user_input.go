210a2d584102c39f6d2ea0fbefe872ed7039e7b4
unknown <joediroberto@gmail.com>
2024-07-17 17:07:32 -0400

    Basic animation embeeded in userSystem

--- systems/user_input.go
+++ systems/user_input.go
@@ -13,23 +13,32 @@ func (uis *UserInputSystem) Update(entities []*entities.Entity) {
 	for _, entity := range entities {
 		// Checks for players that have position and velocity
 		if entity.HasComponent(components.PlayerComponentID) {
+			sprite := entity.GetComponent(components.SpriteComponentID).(*components.SpriteComponent)
 			velocity := entity.GetComponent(components.VelocityComponentID).(*components.VelocityComponent)
 
+			// Control movement
 			if ebiten.IsKeyPressed(ebiten.KeyUp) {
+				sprite.X, sprite.Y, sprite.X1, sprite.Y1 = 16, 16, 32, 32
 				velocity.VY = -2
 			} else if ebiten.IsKeyPressed(ebiten.KeyDown) {
+				sprite.X, sprite.Y, sprite.X1, sprite.Y1 = 0, 0, 16, 16
 				velocity.VY = 2
 			} else {
 				velocity.VY = 0
 			}
 
-			if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+			if ebiten.IsKeyPressed(ebiten.KeyLeft) && !ebiten.IsKeyPressed(ebiten.KeyUp) && !ebiten.IsKeyPressed(ebiten.KeyDown) {
+				sprite.X, sprite.Y, sprite.X1, sprite.Y1 = 32, 32, 48, 48
 				velocity.VX = -2
-			} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
+			} else if ebiten.IsKeyPressed(ebiten.KeyRight) && !ebiten.IsKeyPressed(ebiten.KeyUp) && !ebiten.IsKeyPressed(ebiten.KeyDown) {
+				sprite.X, sprite.Y, sprite.X1, sprite.Y1 = 48, 48, 64, 64
 				velocity.VX = 2
 			} else {
 				velocity.VX = 0
 			}
+
+			// Switch Sprites
+
 		}
 	}
 }
